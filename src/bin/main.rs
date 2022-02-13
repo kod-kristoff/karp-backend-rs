43b9f491b010ce52de9fba18147cbd3ebb8038fd
Kristoffer Andersson <kod.kristoff@gmail.com>
2022-02-13 20:33:11 +0100

    refactor

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,22 +1,30 @@
 use std::net::TcpListener;
 
-use sqlx::MySqlPool;
+use sqlx::MySqlPoolOptions;
 
 use webapp::{
     configuration::get_configuration,
-    startup
+    startup,
+    telemetry,
 };
 
 #[tokio::main]
 async fn main() {
-    let configuration = get_configuration().expect("Failed to read configuration");
+    let subscriber = telemetry::get_subscriber("karp".into(), "info".into(), std::io::stdout);
+    telemetry::init_subscriber(subscriber);
 
-    let connection_pool = MySqlPool::connect(&configuration.database.connection_string())
-        .await
-        .expect("Failed to connect to Mariadb.");
-    let addr = format!("127.0.0.1:{}", configuration.application_port);
-    let listener = TcpListener::bind(&addr).unwrap();
-    tracing::info!("listening on {}", addr);
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_pool = MySqlPoolOptions::new()
+        .connect_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy_with(configuration.database.with_db());
+    // TCP listener
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port,
+    );
+    let listener = TcpListener::bind(&address).unwrap();
+    tracing::info!("listening on {}", address);
+    
     if let Err(e) = startup::run(listener, connection_pool).await {
         eprintln!("Server error: {}", e);
     }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -75,6 +75,22 @@ async fn create_resource_returns_a_400_when_data_is_missing() {
 }
 
 
+use once_cell::sync::Lazy;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
+
+static TRACING: Lazy<()> = Lazy::new(|| {
+    let default_filter_level = "info".to_string();
+    let subscriber_name = "test".to_string();
+
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::stdout);
+        init_subscriber(subscriber);
+    } else {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::sink);
+        init_subscriber(subscriber);
+    };
+});
+
 
 pub struct TestApp {
     pub address: String,
@@ -82,6 +98,7 @@ pub struct TestApp {
 }
 
 async fn spawn_app() -> TestApp {
+    Lazy::force(&TRACING);
     let listener = TcpListener::bind("127.0.0.1:0")
         .expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
@@ -92,6 +109,7 @@ async fn spawn_app() -> TestApp {
         .await
         .expect("Failed to connect to Mariadb");
 
+    let db_pool = configure_database(&configuration.database).await;
     let db_pool_clone = db_pool.clone();
     tokio::spawn(async move {
         startup::run(listener, db_pool_clone).await
@@ -101,4 +119,29 @@ async fn spawn_app() -> TestApp {
         address,
         db_pool,
     }
+}
+
+
+
+async fn configure_database(config: &DatabaseSettings) -> DbPool {
+    use sqlx::migrate::MigrateDatabase;
+
+    // Create database
+//     let mut connection = PgConnection::connect_with(&config.without_db())
+//         .await
+//         .expect("Failed to connect");
+//     connection
+//         .execute(&*format!(r#"CREATE DATABASE "{}";"#, config.database_name))
+//         .await
+//         .expect("Failed to create database");
+
+    // Migrate database
+    let pool = MySqlPool::connect_with(config.with_test_db())
+        .await
+        .expect("Failed to connect to Postgres.");
+    sqlx::migrate!("./migrations")
+        .run(&pool)
+        .await
+        .expect("Failed to run migrations!");
+    pool
 }
