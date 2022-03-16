ac14485417934cecc221fc4c109f3f7ac26bb467
Kristoffer Andersson <kristoffer.andersson@gu.se>
2022-03-16 13:15:30 +0100

    fixes

--- migrations/20220120083348_create_resources.sql
+++ migrations/20220120083348_create_resources.sql
@@ -1,19 +1,21 @@
+-- Create op type
+-- CREATE TYPE operation AS ENUM ('ADDED', 'UPDATED', 'DELETED')
 -- Create resources table
 CREATE TABLE resources
 (
     history_id INTEGER NOT NULL PRIMARY KEY,
     entity_id UUID NOT NULL,
-    resource_id VARCHAR(32) NOT NULL,
+    resource_id VARCHAR(64) NOT NULL,
     resource_type VARCHAR(32) NOT NULL,
     entry_repo_id UUID,
     version INT NOT NULL,
     name VARCHAR(100) NOT NULL,
-    config JSON NOT NULL,
+    config JSONB NOT NULL,
     is_published BOOLEAN,
-    last_modified DOUBLE PRECISION NOT NULL,
+    last_modified DOUBLE NOT NULL,
     last_modified_by VARCHAR(100) NOT NULL,
-    message VARCHAR(200) NOT NULL,
-    op ENUM('ADDED', 'UPDATED', 'DELETED') NOT NULL,
+    message VARCHAR(100) NOT NULL,
+    -- op ENUM('ADDED', 'UPDATED', 'DELETED') NOT NULL,
     discarded BOOLEAN,
     CONSTRAINT entity_id_version_unique_constraint UNIQUE (entity_id, version)
 );
--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -2,38 +2,48 @@
 set -x
 set -eo pipefail
 
-# if ! [ -x "$(command -v mysql)" ]; then
-#   echo >&2 "Error: mysql is not installed."
-# exit 1
+if ! [ -x "$(command -v psql)" ]; then
+  echo >&2 "Error: psql is not installed."
+exit 1
 # fi
+
 if ! [ -x "$(command -v sqlx)" ]; then
   echo >&2 "Error: sqlx is not installed."
   echo >&2 "Use:"
-  echo >&2 "    cargo install --version=0.5.10 sqlx-cli --no-default-features --features mysql"
+  echo >&2 "    cargo install --version=0.5.11 sqlx-cli --no-default-features --features postgres"
   echo >&2 "to install it."
   exit 1
 fi
 
 # Check if a custom user has been set, otherwise default to 'mariadb'
-DB_USER=${MARIADB_USER:=mariadb}
+DB_USER=${POSTGRES_USER:=postgres}
 # Check if a custom password has been set, otherwise default to 'password'
-DB_PASSWORD="${MARIADB_PASSWORD:=password}"
+DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
 # Check if a custom database name has been set, otherwise default to 'karp'
-DB_NAME="${MARIADB_DB:=karp}"
+DB_NAME="${POSTGRES_DB:=karp}"
 # Check if a custom port has been set, otherwise default to '5432'
-DB_PORT="${MARIADB_PORT:=23306}"
+DB_PORT="${POSTGRES_PORT:=25434}"
 
+# Allow to skip Docker if a dockerized Postgres database is already running
 if [[ -z "${SKIP_DOCKER}" ]]
 then
+  # if a postgres container is running, print instructions to kill it and exit
+  RUNNING_POSTGRES_CONTAINER=$(docker ps --filter 'name=postgres' --format '{{.ID}}')
+  if [[ -n $RUNNING_POSTGRES_CONTAINER ]]; then
+    echo >&2 "there is a postgres container already running, kill it with"
+    echo >&2 "    docker kill ${RUNNING_POSTGRES_CONTAINER}"
+    exit 1
+  fi
+  # Launch postgres using Docker
   docker run \
-      --detach \
-      -e MYSQL_RANDOM_ROOT_PASSWORD=1 \
-      -e MARIADB_USER=${DB_USER} \
-      -e MARIADB_PASSWORD=${DB_PASSWORD} \
-      -e MARIADB_DATABASE=${DB_NAME} \
-      -p "${DB_PORT}":3306 \
-      --name mariadb_karp_rs \
-      mariadb:10.7 # -N 1000 # ^ Increased maximum number of connections for testing purposes
+      -e POSTGRES_USER=${DB_USER} \
+      -e POSTGRES_PASSWORD=${DB_PASSWORD} \
+      -e POSTGRES_DB=${DB_NAME} \
+      -p "${DB_PORT}":5432 \
+      -d \
+      --name "postgres_$(date '+%s')" \
+      postgres -N 1000
+      # ^ Increased maximum number of connections for testing purposes
 fi
 
 function is_mysql_alive() {
@@ -54,16 +64,16 @@ do
   echo "Waiting for MySQL to be ready..."
 done
 
-# export MYSQL_PWD="${DB_PASSWORD}"
-# until mysql -h "localhost" -P${DB_PORT} -u"${DB_USER}" -p"${DB_PASSWORD}" mariadb -c '\q'; do
-#     >&2 echo "Mariadb is still unavailable - sleeping"
-#     sleep 1
-# done
+# Keep pinging Postgres until it's ready to accept commands
+until PGPASSWORD="${DB_PASSWORD}" psql -h "${DB_HOST}" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
+  >&2 echo "Postgres is still unavailable - sleeping"
+  sleep 1
+done
 
->&2 echo "Mariadb is up and running on port ${DB_PORT}"
+>&2 echo "Postgres is up and running on port ${DB_PORT} - running migrations now!"
 
-export DATABASE_URL=mysql://${DB_USER}:${DB_PASSWORD}@127.0.0.1:${DB_PORT}/${DB_NAME}
+export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
 sqlx database create
 sqlx migrate run
 
->&2 echo "Mariadb has been migrated, ready to go!"
+>&2 echo "Postgres has been migrated, ready to go!"
